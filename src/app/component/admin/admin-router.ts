215c6c6d1842cf698ef9b76f7565b4402c272e77
ManhNguyen0811 <manhnguyen0916697145@gmail.com>
2024-12-30 17:48:49 +0700

    add layout-admin, menu-admin, 20% category-admin

--- src/app/component/admin/admin-router.ts
+++ src/app/component/admin/admin-router.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 import { AdminComponent } from "./admin.component";
 import { UserComponent } from "./user/user.component";
+import { CategoryComponent } from "./category/category.component";
  
 export const adminRouter: Routes =[
     {
@@ -11,6 +12,10 @@ export const adminRouter: Routes =[
             {
                 path:'user',
                 component: UserComponent
+            },
+            {
+                path:'category',
+                component: CategoryComponent
             }
          ]
     }
