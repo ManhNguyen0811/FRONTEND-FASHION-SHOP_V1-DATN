88db6ea052b9c4ff726ba507bc7ca3b72f6db282
Phatcr812 <chauphat176@gmail.com>
2025-01-03 16:53:01 +0700

    add footer and responsive

--- src/app/component/client/client.component.ts
+++ src/app/component/client/client.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NavBottomComponent } from "./nav-bottom/nav-bottom.component";
-import { HttpClientModule} from '@angular/common/http';
+import {FooterComponent} from './footer/footer.component';
 
 @Component({
   selector: 'app-client',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, HeaderComponent, NavBottomComponent,HttpClientModule],
+  imports: [RouterLink, RouterOutlet, HeaderComponent, NavBottomComponent, FooterComponent],
   templateUrl: './client.component.html',
   styleUrl: './client.component.scss'
 })
