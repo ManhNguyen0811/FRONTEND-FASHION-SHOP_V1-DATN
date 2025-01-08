69c26202375fda1e376b86d061b355d2fae34f83
Phatcr812 <chauphat176@gmail.com>
2025-01-08 16:30:41 +0700

    befort pull

--- src/app/component/client/login/login.component.ts
+++ src/app/component/client/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import {NavigationService} from '../../../services/Navigation/navigation.service';
+import {TranslateModule} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,TranslateModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
