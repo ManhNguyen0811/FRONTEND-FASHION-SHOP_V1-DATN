fba2d1cf4000f1edff9ea54c4a338370665c09aa
Phatcr812 <chauphat176@gmail.com>
2025-01-21 18:13:37 +0700

    update category

--- src/app/component/client/nav-bottom/nav-bottom.component.ts
+++ src/app/component/client/nav-bottom/nav-bottom.component.ts
@@ -43,10 +43,17 @@ export class NavBottomComponent {
   }
 
   categories = [
-    { name: 'ĐỒ MẶC NGOÀI', img: 'assets/images/thoitrangnu.jpg' },
-    { name: 'QUẦN', img: 'assets/images/thoitrangnu.jpg' },
-    { name: 'ĐỒ MẶC NHÀ', img: 'assets/images/thoitrangnu.jpg' },
-    { name: 'PHỤ KIỆN', img: 'assets/images/hinhDoc1.webp' },
+    { name: 'ĐỒ MẶC NGOÀI', img: 'https://im.uniqlo.com/global-cms/spa/resf71a16ca69716ea0d7806171a1551d22fr.png',
+      children: [
+        { name: 'Áo Blouson & Áo Parka', img: 'https://im.uniqlo.com/global-cms/spa/res1017a8095aa156c19814acc6c133c1a3fr.jpg' },
+        { name: 'Áo Down', img: 'https://im.uniqlo.com/global-cms/spa/res86899907a6bd709e533c98886cfa79d0fr.png' },
+        { name: 'Áo Khoác & Áo Blazer', img: 'https://im.uniqlo.com/global-cms/spa/res54683d0d8105329d34b0faa277fc1651fr.jpg' },
+        { name: 'Tất Cả ĐỒ MẶC NGOÀI', img: 'https://im.uniqlo.com/global-cms/spa/res1fb3deb971f48bb35df1afdcec064c6cfr.jpg' }
+      ]
+    },
+    { name: 'QUẦN', img: 'https://im.uniqlo.com/global-cms/spa/resd5da71558f0752d65dd506a2e5f11e38fr.png' },
+    { name: 'ĐỒ MẶC NHÀ', img: 'https://im.uniqlo.com/global-cms/spa/resb64771b00f5cd167bfb4016c05133d1afr.png' },
+    { name: 'PHỤ KIỆN', img: 'https://im.uniqlo.com/global-cms/spa/resf8cea0298a7dc731c3904021ee06995ffr.jpg' },
     { name: 'HEATTECH', img: 'assets/heattech.png' },
     { name: 'UT: Áo Thun', img: 'assets/ut.png' },
     { name: 'AIRism', img: 'assets/airism.png' },
@@ -54,4 +61,21 @@ export class NavBottomComponent {
     { name: 'KHUYẾN MÃI', img: 'assets/limited.png' },
     { name: 'SẮP MỞ BÁN', img: 'assets/comingsoon.png' }
   ];
+
+  currentLevel: number = 2;
+  selectedCategory: any = null;
+
+// Hàm xử lý khi chọn danh mục ở tầng 2
+  selectCategory(category: any): void {
+    if (category.children) {
+      this.selectedCategory = category;
+      this.currentLevel = 3; // Chuyển sang tầng 3
+    }
+  }
+
+// Hàm quay lại tầng 2
+  goBack(): void {
+    this.currentLevel = 2; // Quay lại tầng 2
+  }
+
 }
