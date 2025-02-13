c862a0bed3cabff2a8bf8a2e2555f6639e02b7d9
Phatcr812 <chauphat176@gmail.com>
2025-02-13 15:32:53 +0700

    save before pull

--- src/app/component/client/home/home.component.ts
+++ src/app/component/client/home/home.component.ts
@@ -1,6 +1,13 @@
 import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
+import {Component, HostListener, Inject, OnInit, PLATFORM_ID} from '@angular/core';
 import {NavBottomComponent} from '../nav-bottom/nav-bottom.component';
+import {Router} from '@angular/router';
+import {NavigationService} from '../../../services/Navigation/navigation.service';
+import {BannerService} from '../../../services/client/BannerService/banner.service';
+import {catchError, firstValueFrom, map, Observable, of} from 'rxjs';
+import {BannerDTO} from '../../../models/BannerDTO';
+import {response} from 'express';
+import {ApiResponse} from '../../../dto/Response/ApiResponse';
 
 @Component({
   selector: 'app-home',
@@ -9,13 +16,28 @@ import {NavBottomComponent} from '../nav-bottom/nav-bottom.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+export class HomeComponent implements OnInit {
+  currentLang: string = ''; // Ngôn ngữ mặc định
+  currentCurrency: string = ''; // Tiền tệ mặc định
+  url: string = '';
+  banners: Observable<BannerDTO[] | null> = of([]);
 
-  ngOnInit(): void {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object,
+              private router: Router,
+              private navigationService: NavigationService,
+              private bannerService: BannerService,) {
+
+  }
+
+  async ngOnInit(): Promise<void> {
     if (isPlatformBrowser(this.platformId)) {
       this.handleScroll(); // Gọi xử lý ban đầu khi load trang
     }
+
+    this.currentLang = await firstValueFrom(this.navigationService.currentLang$);
+    this.currentCurrency = await  firstValueFrom(this.navigationService.currentCurrency$);
+    this.banners =  this.getBanners(this.currentLang);
+    console.log(this.banners);
   }
 
   // Lắng nghe sự kiện scroll trên toàn bộ cửa sổ
@@ -41,4 +63,12 @@ export class HomeComponent {
       }
     });
   }
+
+  getBanners(lang: string): Observable<BannerDTO[] | null> {
+    return this.bannerService.getBanners(lang).pipe(
+      map((response: ApiResponse<BannerDTO[]>)=> response.data || []),
+      catchError(() => of(null))
+    )
+  }
+
 }
