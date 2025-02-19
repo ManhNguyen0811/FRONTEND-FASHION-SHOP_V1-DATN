58367a1bc5d13560ce08f0064894bc44c6b80d8d
ManhNguyen0811 <manhnguyen0916697145@gmail.com>
2025-02-19 15:32:08 +0700

    done fiter , add api category select

--- src/app/component/admin/categoty/list-category/list-category.component.ts
+++ src/app/component/admin/categoty/list-category/list-category.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HeaderAdminComponent } from '../../header-admin/header-admin.component';
 import { TableComponent } from '../../table/table.component';
 import { PageResponse } from '../../../../dto/Response/page-response';
- 
+
 import { CategoryAdminService } from '../../../../services/admin/CategoryService/category.service';
 import { catchError, firstValueFrom, forkJoin, map, Observable, of } from 'rxjs';
 import { ApiResponse } from '../../../../dto/Response/ApiResponse';
@@ -10,6 +10,7 @@ import { CommonModule } from '@angular/common';
 import { CategoryAdminDTO } from '../../../../dto/CategoryAdminDTO';
 import { ToastrService } from 'ngx-toastr';
 import { CategoryAdmin } from '../../../../models/Category/CategotyAdmin';
+import { FormsModule } from '@angular/forms';
 
 export interface TableDataModel {
   id: number;
@@ -24,67 +25,123 @@ export interface TableDataModel {
 @Component({
   selector: 'app-list-category',
   standalone: true,
-  imports: [HeaderAdminComponent, TableComponent,CommonModule],
+  imports: [HeaderAdminComponent, TableComponent, CommonModule, FormsModule],
   templateUrl: './list-category.component.html',
   styleUrl: './list-category.component.scss'
 })
 
 
 export class ListCategoryComponent implements OnInit {
-  headers: string[] = ['id', 'name', 'imageUrl', 'isActive', 'parentId', 'parentName', 'createdAt', 'updatedAt', 'button'];
+  headers: string[] = ['id', 'name', 'imageUrl', 'isActive', 'parentId', 'parentName', 'createdAt', 'updatedAt', 'createdBy',
+    'updatedBy', 'button'];
 
   checkedItems: any[] = [];  // Danh sách các mục đã chọn, đã được khởi tạo
 
   page: number = 0
   size: number = 7
-  sortBy: string = 'createdAt'
+  nameSearch: string = ''
+  parentIdSearch: any = null
+  sortBy: string = 'id'
   sortDir: string = 'desc'
+  isActive: any = null
 
+  debounceTimerName: any;
+  debounceTimerParentId: any;
   dataPageCategory: PageResponse<CategoryAdmin[]> | null = null
-  dataCategories: CategoryAdmin[]  = [];
+  dataCategories: CategoryAdmin[] = [];
 
 
 
 
   constructor(
     private categoryAdminService: CategoryAdminService,
-    private toastService : ToastrService
+    private toastService: ToastrService
   ) {
 
   }
 
   async ngOnInit(): Promise<void> {
-    this.fetchCategory()
+    if (this.isActive === null) {
+      this.fetchCategory()
 
-
+    }
+    this.fetchCategory();
   }
   onPageChange(newPage: number): void {
     this.page = newPage;  // Cập nhật giá trị page
     this.fetchCategory(); // Gọi lại API với trang mới
   }
 
-  
+  onCreateAtChange() {
+    this.fetchCategory();
+
+  }
+  onSortDirChange() {
+    this.fetchCategory();
+
+  }
+  onIsActiveChange(): void {
+    this.fetchCategory()
+    console.log("Selected isActive value:", this.isActive);
+  }
+
   async fetchCategory(): Promise<void> {
 
     const callApis = {
-      dataCategories: this.getCategories(this.page, this.size, this.sortBy, this.sortDir).pipe(catchError(() => of(null)))
+      dataCategories: this.getCategories(this.page, this.size, this.sortBy, this.sortDir, this.nameSearch,
+         this.isActive,this.parentIdSearch).pipe(catchError(() => of(null)))
     }
 
     const response = await firstValueFrom(forkJoin(callApis))
     this.dataPageCategory = response.dataCategories
-    this.dataCategories = response.dataCategories?.content?.flat() || [];
+    console.log(" run  fetchCategory")
 
-    console.log("dataPageCategory : " + this.dataPageCategory)
+    // this.dataCategories = response.dataCategories?.content?.flat() || [];
 
+    // console.log("dataPageCategory : " + this.dataCategories[0].updatedBy)
+
+  }
+  onNameChange(value: string): void {
+    // Xóa timer cũ nếu có
+    if (this.debounceTimerName) {
+      clearTimeout(this.debounceTimerName);
+    }
+    // Đặt timer mới chờ 1s
+    this.debounceTimerName = setTimeout(() => {
+      this.searchNameCategory(value);
+    }, 1000);
+  }
+  onParentIdChange(value: string): void {
+    // Xóa timer cũ nếu có
+    if (this.debounceTimerParentId) {
+      clearTimeout(this.debounceTimerParentId);
+    }
+    // Đặt timer mới chờ 1s
+    this.debounceTimerParentId = setTimeout(() => {
+      this.searchParentIdCategory(value);
+    }, 1000);
+  }
+  searchParentIdCategory(value: string): void {
+    this.parentIdSearch = value
+    this.fetchCategory()
+ 
+  }
+  searchNameCategory(value: string): void {
+    this.nameSearch = value
+    this.fetchCategory()
+    console.log(" run  searchNameCategory")
+    console.log(this.nameSearch)
   }
 
   getCategories(
     page: number,
     size: number,
     sortBy: string,
-    sortDir: string
+    sortDir: string,
+    name: string, isActive: boolean,
+    parentId :number
   ): Observable<PageResponse<CategoryAdmin[]>> {
-    return this.categoryAdminService.getCategoriesAdmin(page, size, sortBy, sortDir).pipe(
+    return this.categoryAdminService.getCategoriesAdmin(page, size, sortBy, sortDir, name, isActive,parentId).pipe(
       map((response: ApiResponse<PageResponse<CategoryAdmin[]>>) => response.data || null),
       catchError(() => of(null as any))
     )
@@ -98,7 +155,7 @@ export class ListCategoryComponent implements OnInit {
 
   toggleCheckbox(item: any) {
     if (!Array.isArray(this.checkedItems)) {
-      this.checkedItems = [];  
+      this.checkedItems = [];
     }
 
     item.checked = !item.checked;
