aaba11084340fa6bac5fdea57107f57cf4e720b0
Tai <bvt.vantai@gmail.com>
2025-02-25 14:14:15 +0700

    update OrderHistory

--- src/app/services/client/OrderHistoryService/order-history.service.ts
+++ src/app/services/client/OrderHistoryService/order-history.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {catchError, map, Observable, throwError} from 'rxjs';
+import {ApiResponse} from '../../../dto/Response/ApiResponse';
+import { OrderHistoryDTO } from '../../../models/OrderHistory/OrderHistory';
+
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +15,49 @@ export class OrderHistoryService {
 
   constructor(private http: HttpClient) {}
 
-  getOrderHistory(userId: number, page: number = 0, size: number = 5): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/${userId}?page=${page}&size=${size}`);
+  getOrderHistory(userId: number, page: number = 0, size: number = 5): Observable<{
+    content: OrderHistoryDTO[];
+    totalElements: number
+  }> {
+    return this.http.get<ApiResponse<{ content: OrderHistoryDTO[], totalElements: number }>>(
+      `${this.apiUrl}/${userId}?page=${page}&size=${size}`
+    )
+      .pipe(
+        map(response => {
+          return {
+            content: response.data.content,
+            totalElements: response.data.totalElements
+          };
+        }),
+        catchError(error => {
+          console.error('Lỗi khi lấy lịch sử đơn hàng:', error);
+          return throwError(() => new Error('Không thể lấy dữ liệu lịch sử đơn hàng.'));
+        })
+      );
   }
+
+
+  getOrderHistoryByStatus(status: string, page: number = 0, size: number = 5): Observable<{
+    content: OrderHistoryDTO[];
+    totalElements: number;
+  }> {
+    return this.http.get<ApiResponse<{ content: OrderHistoryDTO[], totalElements: number }>>(
+      `${this.apiUrl}/status?status=${status}&page=${page}&size=${size}`
+    )
+      .pipe(
+        map(response => {
+          return {
+            content: response.data.content,
+            totalElements: response.data.totalElements
+          };
+        }),
+        catchError(error => {
+          console.error('Lỗi khi lấy lịch sử đơn hàng theo trạng thái:', error);
+          return throwError(() => new Error('Không thể lấy dữ liệu lịch sử đơn hàng theo trạng thái.'));
+        })
+      );
+  }
+
+
+
 }
